1bd5de2495989047adf254d69709d961dca0ee8b
SolGee <solsv00@hotmail.com>
2019-12-02 16:00:36 -0600

    Updates

---
+++ precache-manifest.0fe27d10b8ccc6f96520f099c0bf6c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057498cb499f9924663661bde38e9236",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1287149841e19c0a6a02",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7bb673dc08d3fc88cd44",
+    "url": "./static/js/2.a498f86a.chunk.js"
+  },
+  {
+    "revision": "1287149841e19c0a6a02",
+    "url": "./static/js/main.5535aa53.chunk.js"
+  },
+  {
+    "revision": "0c30d0b97221def4146b",
+    "url": "./static/js/runtime-main.69620498.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
