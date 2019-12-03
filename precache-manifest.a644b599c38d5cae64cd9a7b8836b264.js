f3b5d64ca1a2c679d2391dfa051ce6c0d91b3430
SolGee <solsv00@hotmail.com>
2019-12-03 14:54:49 -0600

    Updates

--- precache-manifest.a644b599c38d5cae64cd9a7b8836b264.js
+++ precache-manifest.a644b599c38d5cae64cd9a7b8836b264.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ba671853f913c70a5d891755b6c6bd27",
+    "revision": "3f35ba6e9d32c79d7330dc57306ac131",
     "url": "/MEX008-Accenture-Talent-Fest/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MEX008-Accenture-Talent-Fest/static/css/2.e315d21c.chunk.css"
   },
   {
-    "revision": "5238157b1480f2c8e277",
-    "url": "/MEX008-Accenture-Talent-Fest/static/css/main.5df9b99d.chunk.css"
+    "revision": "89b56c2d4c7bf4d4cb06",
+    "url": "/MEX008-Accenture-Talent-Fest/static/css/main.274d0fde.chunk.css"
   },
   {
     "revision": "bcfb940f0ab7d5499a5b",
     "url": "/MEX008-Accenture-Talent-Fest/static/js/2.2c5bdf77.chunk.js"
   },
   {
-    "revision": "5238157b1480f2c8e277",
-    "url": "/MEX008-Accenture-Talent-Fest/static/js/main.af063445.chunk.js"
+    "revision": "89b56c2d4c7bf4d4cb06",
+    "url": "/MEX008-Accenture-Talent-Fest/static/js/main.fb20b9f3.chunk.js"
   },
   {
     "revision": "3c3ad50d287543ef8744",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MEX008-Accenture-Talent-Fest/precache-manifest.28389df827fc7f38a6a8d77ac099b9d8.js"
+  "/MEX008-Accenture-Talent-Fest/precache-manifest.a644b599c38d5cae64cd9a7b8836b264.js"
 );
 
 self.addEventListener('message', (event) => {
